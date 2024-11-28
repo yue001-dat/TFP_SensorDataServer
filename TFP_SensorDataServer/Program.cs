d4e8e72d6bd326404cf684b0b1b13a320616ae9a
yue001 <yue001@edu.zealand.dk>
2024-11-28 11:37:24 +0100

    Add project files.

---
+++ TFP_SensorDataServer/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Text.Json;
+using TFP_SensorDataLib;
+
+Console.WriteLine("Receiving Sensor Data (UDP)");
+
+int port = 65000;
+UdpClient socket = new UdpClient();
+socket.Client.Bind(new IPEndPoint(IPAddress.Any, port));
+SensorRepository _repo = new SensorRepository();
+
+while (true)
+{
+	IPEndPoint from = null;
+
+	byte[] data = socket.Receive(ref from);
+	string dataString = Encoding.UTF8.GetString(data);
+
+	// We need some sort of validation - if the data is not correctly formatted as JSON our script fails.
+	SensorData sensorData = JsonSerializer.Deserialize<SensorData>(dataString);
+	_repo.Add(sensorData);
+
+	Console.WriteLine("Data Received " + from.Address);
+}
